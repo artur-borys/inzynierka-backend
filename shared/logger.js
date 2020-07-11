0b73c13f0399e683d370080304968175ca066e46
Artur Boryś <artur.borys13@gmail.com>
2020-07-11 15:16:33 +0200

    fine tune log

--- shared/logger.js
+++ shared/logger.js
@@ -1,27 +1,35 @@
-const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
+const { combine, json, timestamp, prettyPrint } = format;
 const expressWinston = require("express-winston");
 
-const combined_transport = new winston.transports.File({
+const combined_transport = new transports.File({
   filename: 'combined.log',
   level: 'info'
 })
 
-const error_transport = new winston.transports.File({
+const error_transport = new transports.File({
   filename: 'error.log',
   level: 'error'
 })
 
-module.exports.logger = winston.createLogger({
+const mFormat = combine(timestamp(), json(), prettyPrint())
+
+module.exports.logger = createLogger({
   transports: [combined_transport, error_transport],
-  format: winston.format.json()
+  format: mFormat
 })
 
 module.exports.expressLogger = expressWinston.logger({
   transports: [combined_transport],
-  format: winston.format.json()
+  format: mFormat,
+  dynamicMeta: (req, res) => {
+    return {
+      ip: req.ip
+    }
+  }
 })
 
 module.exports.expressErrorLogger = expressWinston.errorLogger({
   transports: [error_transport],
-  format: winston.format.json()
+  format: mFormat
 })
