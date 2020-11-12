2fbaa8353d8eb3a85472cec70888be7ac95821f4
Artur Borys <artur.borys13@gmail.com>
2020-11-12 18:54:47 +0100

    i dunno

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   host: "0.0.0.0",
   port: 4000,
   mongo: {
-    url: "mongodb://192.168.253.129/first-aid-help",
+    url: "mongodb://192.168.57.128/first-aid-help",
     options: { useNewUrlParser: true, useUnifiedTopology: true }
   },
   admin: {
